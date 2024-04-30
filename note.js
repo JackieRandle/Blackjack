1bdfeb8553eca7ea3b647218460b234723c9e353
JackieRandle <159210475+JackieRandle@users.noreply.github.com>
2024-04-29 20:46:56 -0500

    BlackJack

---
+++ note.js
@@ -0,0 +1,61 @@
+let firstCard = 6
+let secondCard = 10
+
+let sum = firstCard + secondCard 
+
+console.log(sum)
+
+if (sum < 21) {
+    console.log("do you want to draw a new card?")
+
+} else if (sum === 21) {
+    console.log("Wohoo! You're got Blackjack! ")
+
+} else {
+    console.log("You're out of the game!  ")
+
+
+}
+
+let age = 100
+if (age < 100) {
+    console.log("Not elegible")
+
+}else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+
+}else {
+    console.log("Not elegible, you have aleady gotten one")
+
+}
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? ")
+
+}else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! ")
+
+}else {
+    console.log("You're out of the game!")
+}
+let age = 100
+// if (age < 100) {
+//     console.log("Not elegible")
+
+// }else if (age === 100) {
+//     console.log("Here is your birthday card from the King!")
+
+// }else {
+//     console.log("Not elegible, you have aleady gotten one")
+
+// }
+
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new card? ")
+
+// }else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! ")
+
+// }else {
+//     console.log("You're out of the game!")
+// }
---
+++ script.js
@@ -0,0 +1,50 @@
+// console.log(4 === 3) false
+// console.log(5 > 2) true
+// console.log(12 > 12) false
+// console.log(3 < 0) false
+// console.log(3 >= 3) true 
+// console.log(11 <= 11) true
+// console.log(3 <= 2) false
+
+
+let message = ""
+let message = "we're logging out"
+console.log("we're logging out")
+
+
+
+
+
+
+
+
+// let secondCard = 11
+
+// let sum = firstCard + secondCard 
+// let hasBlackJack = false
+// let isAlive = true
+
+// console.log(sum)
+
+// if (sum <= 20) {
+//     message = ("Do you want to draw a new card?")
+
+//       }
+
+// } else if (sum === 21) {
+//     message = ("Wohoo! You're got Blackjack! ")
+//     hasBlackJack = true
+// } else {
+//     message = ("You're out of the game!  ")
+//     isAlive = false
+
+
+// }
+console.log(message)
+
+// // Cash Out!
+
+// console.log(isAlive)
+// console.log(hasBackJack)
+
+// // 

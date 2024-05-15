2c5f20249e96cf911498d0b91149e746e992bbde
JackieRandle <159210475+JackieRandle@users.noreply.github.com>
2024-05-14 20:12:52 -0500

    BlackJack

--- note.js
+++ note.js
@@ -1,40 +1,57 @@
-let firstCard = 3
-let secondCard = 5
-let sum = firstCard + secondCard
+let player = {
+    name: "Jacqueline",
+    chips: 200
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
-let cards = [firstCard, secondCard]
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-function startGane() {
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
-function renderGame(){
-    if(sum <= 20){
-        message = "Would you like to draw again?"
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
-    else if (sum === 21){
-        message = "BLACKJACK!"
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
         hasBlackJack = true
-    }
-    else if (sum > 21) {
-        message = "sorry,play again?"
+    } else {
+        message = "You're out of the game!"
         isAlive = false
     }
-    messageEl.textContent=message
-    sumEl.textContent= "Sum: "+sum
-    cardsEl.textContent = "Cards: " + cards[0] + " " + Cards[1] + thirdCard
+    messageEl.textContent = message
 }
 function newCard() {
-    console.log("Drawing a new Card from the deck!")
-    let card = 7
-    sum += card
-    renderGame() 
-    
-
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
     }
-
-
+}
--- script.js
+++ script.js
@@ -35,3 +35,64 @@ let messages = [
 
     messages.pop() remove an items
     console.log(messages)
+
+    for (let i = 10; i <101; i +=10) {
+        console.log(i)
+    }
+
+
+        let cards =[7, 3, 9]
+
+        for(let cards = 7, 3, 9; cards <10, count +=3) {
+            console.log(cards)
+        }
+
+        let sentence =  ["Hello ","my ", "name ", "is ", "Per"]
+        let greetingEl = document.getElementById("greeting-el")
+
+        for (let i =0; i < sentence.length; i++) [
+
+            greetingEl.textConent += sentence[i]
+
+        ]
+
+
+        let player1Time = 102
+let player2Time = 107
+let fastestRace = getFastestRaceTine(
+let totalRaceTime= player1Time + player2Time
+    console.log(fastestRace)
+)
+
+function totalRaceTime() {
+    return  player1Time + player2Time
+}
+    console.log(fastestRace)
+
+
+    let 
+function getFastestRaceTime() {
+    if (player1Time < player2Time) {
+        return player1Time
+
+    } else if (player2Time < player1Time) {
+        return player2Time
+    } else {
+        return player1Time
+
+    }
+    let randomNumber = Math.floor(Math.random() * 6 ) + 1
+        
+
+        console.log(randomNumber)
+    
+
+        let flooredNumber = Math.floor(3.45632)
+
+        console.log(flooredNumber)
+
+        function rollDice() {
+            let randomNumber = Math.floor(Math.random() * 6 ) + 1
+            return randomNumber
+        }
+        console.log(randomNumber)
